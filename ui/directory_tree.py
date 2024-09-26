eb26f85b98a5e7f3773bf87ac05135cd7f1a042a
jim <createzn@gmail.com>
2024-09-26 18:17:45 +0800

    refine code

--- ui/directory_tree.py
+++ ui/directory_tree.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk
 from config import constant
+from ui.syle.tree_view_style_manager import TreeViewStyleManager
 from util import image_util
 
 
@@ -16,3 +17,5 @@ class DirectoryTree:
         self.closed_folder_resized_icon = ImageTk.PhotoImage(closed_folder_resized)
         open_folder_resized = image_util.resize_image_by_path(constant.OPEN_FOLDER_IMAGE, (20, 20))
         self.open_folder_resized_icon = ImageTk.PhotoImage(open_folder_resized)
+        self.style_manager = TreeViewStyleManager(self.tree)
+        self.style_manager.set_tree_style()
