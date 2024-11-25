2e2e5f0fbad74cbe964c048c3ac8723a03c16985
jim <455428781@qq.com>
2024-11-25 10:51:12 +0800

    change paned window to ttk style

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+
 from config.enum import ViewType
 from ui.directory_tree import DirectoryTree
 from ui.display_frame import DisplayFrame
@@ -18,12 +20,12 @@ class MainWindow:
 
     def create_ui(self, root):
         self.input_frame = InputFrame(root)
-        self._paned_window = tk.PanedWindow(root, orient=tk.HORIZONTAL, sashwidth=10)
+        self._paned_window = ttk.PanedWindow(root, orient=tk.HORIZONTAL)
         self._paned_window.pack(fill=tk.BOTH, expand=True, padx=20, pady=(20, 5))
         self.directory_tree = DirectoryTree(self._paned_window)
         self.display_frame = DisplayFrame(self._paned_window)
         self.output_window = OutputWindow(root)
-        self._paned_window.add(self.directory_tree.left_frame, stretch="always")
-        self._paned_window.add(self.display_frame.right_frame, stretch="always")
+        self._paned_window.add(self.directory_tree.left_frame, weight=1)
+        self._paned_window.add(self.display_frame.right_frame, weight=6)
         self._paned_window.update_idletasks()
-        self._paned_window.sash_place(0, 150, 0)
+        #self._paned_window.sash_place(0, 150, 0)
