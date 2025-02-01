b698e9449b5aa34be14b2119990fc7c1a006ab6c
jim <455428781@qq.com>
2025-02-01 11:43:04 +0800

    change the style of the paned_window from tk to ttk

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,15 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
 
+from ttkbootstrap import Style
+
 from config.enum import ViewType
+from ui.advanced_log_window import AdvancedLogWindow
 from ui.directory_tree import DirectoryTree
 from ui.display_frame import DisplayFrame
 from ui.input_frame import InputFrame
 from ui.output_window import OutputWindow
+from ui.setting_window import SettingsWindow
+from ui.top_bar import TopBar
 
 
 class MainWindow:
     def __init__(self, root):
+        self.settings_window = None
+        self.advanced_log_window = None
+        self.top_bar = None
         self._paned_window = None
         self.root = root
         self.output_window = None
@@ -17,15 +25,18 @@ class MainWindow:
         self.input_frame = None
         self.directory_tree = None
         self.create_ui(root)
-
+        self.style = Style(theme="flatly")
     def create_ui(self, root):
+        self.top_bar = TopBar(root)
+        self.settings_window = SettingsWindow(root)
+        self.advanced_log_window = AdvancedLogWindow(root)
         self.input_frame = InputFrame(root)
-        self._paned_window = ttk.PanedWindow(root, orient=tk.HORIZONTAL)
-        self._paned_window.pack(fill=tk.BOTH, expand=True, padx=20, pady=(20, 5))
+        self._paned_window = tk.PanedWindow(root, orient=tk.HORIZONTAL, sashwidth=10)
+        self._paned_window.pack(fill=tk.BOTH, expand=True, padx=20, pady=(5, 5))
         self.directory_tree = DirectoryTree(self._paned_window)
         self.display_frame = DisplayFrame(self._paned_window)
         self.output_window = OutputWindow(root)
-        self._paned_window.add(self.directory_tree.left_frame, weight=1)
-        self._paned_window.add(self.display_frame.right_frame, weight=6)
+        self._paned_window.add(self.directory_tree.left_frame)
+        self._paned_window.add(self.display_frame.right_frame)
         self._paned_window.update_idletasks()
         #self._paned_window.sash_place(0, 150, 0)
