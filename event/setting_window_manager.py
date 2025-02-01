931394a0803ef3adfb41d52ca621b23bae8b0b13
jim <455428781@qq.com>
2025-02-01 11:22:14 +0800

    add setting window events

---
+++ event/setting_window_manager.py
@@ -0,0 +1,185 @@
+from pathlib import Path
+from tkinter import font, messagebox, filedialog
+import tkinter as tk
+
+from exceptiongroup import catch
+
+from api.openai_text_api import OpenaiTextApi
+from config.app_config import AppConfig
+from config.constant import APP_NAME, LOG_FOLDER
+from event.event_bus import event_bus
+from util.file_util import get_documents_directory
+from util.logger import Logger, logger
+from util.window_util import center_window
+from widget.confirm_dialog import ConfirmDialog
+from widget.custom_confirm_dialog import CustomConfirmDialog
+
+
+class SettingWindowManager:
+    def __init__(self, main_window):
+        self.selected_log_directory = None
+        self.settings_window = main_window.settings_window
+        self.app_config = AppConfig()
+        self.root = self.settings_window.root
+        self.main_window = self.settings_window.main_window
+        self.setting_window = self.settings_window
+        self.openai_text_api = OpenaiTextApi()
+        self.bind_events()
+        self.open_setting_if_api_key_is_none()
+
+
+    def bind_events(self):
+        self.setting_window.main_window.protocol("WM_DELETE_WINDOW", self.on_cancel)
+        event_bus.subscribe("UpdateSettingWidget", self.update_setting_widget)
+        event_bus.subscribe("OpenSettingWindow", self.open_setting_window)
+        self.setting_window.confirm_button.config(command = self.on_confirm)
+        self.setting_window.cancel_button.config(command = self.on_cancel)
+        self.setting_window.test_api_key_button.config(command = self.on_click_test_api_key_button)
+        self.setting_window.api_key_input_text.bind("<KeyRelease>", self.check_input_text)
+        self.setting_window.max_token_toggle_button.bind("<<CheckboxToggled>>", self.on_max_token_toggle_change)
+        self.setting_window.select_folder_button.bind("<Button-1>", lambda e: self.select_directory(e))
+        self.setting_window.advanced_button.bind("<Button-1>", lambda e: self.open_advanced_window(e))
+
+
+    def display_max_token_slider(self, state, max_token_count):
+        if state:
+            if max_token_count == '0' or max_token_count == 0:
+                self.settings_window.max_token_slider.set(1000)
+            else:
+                self.settings_window.max_token_slider.set(max_token_count)
+            self.settings_window.max_token_slider.pack(side=tk.LEFT, fill=tk.X, padx=10, pady=5, anchor=tk.E)
+            self.settings_window.max_token_express_label.pack_forget()
+        else:
+            self.settings_window.max_token_slider.pack_forget()
+            self.settings_window.max_token_express_label.pack(side = tk.LEFT, padx=(10, 10), pady=5)
+
+    def on_max_token_toggle_change(self, event):
+        state = self.settings_window.max_token_toggle_button.get_state()
+        self.display_max_token_slider(state, self.setting_window.max_token_slider.get())
+
+    def open_setting_window(self):
+        self.open()
+
+    def open_setting_if_api_key_is_none(self):
+        api_key = self.app_config.get("api_key")
+        if not api_key:
+            self.root.after(500,  self.open)
+
+    def get_system_fonts(self):
+        values = font.families()
+        return values
+
+    def open(self):
+        center_window(self.main_window, self.root,self.setting_window.win_width, self.setting_window.win_height)
+        self.main_window.deiconify()
+        self.main_window.grab_set()
+        self.load_setting()
+
+    def load_setting(self):
+        api_key = self.app_config.get("api_key")
+        max_token = self.app_config.get("max_token", "0")
+        typewriter_effect_state = self.app_config.get("typewriter_effect", "1")
+        current_log_folder = self.app_config.get("log_folder")
+        current_log_state = self.app_config.get("log_state", "0")
+        self.setting_window.api_key_input_text_var.set(value="")
+        if api_key:
+            self.setting_window.api_key_input_text_var.set(value=api_key)
+        self.setting_window.max_token_toggle_button.set_state(max_token != '0')
+        self.setting_window.log_entry.delete(0, tk.END)
+        self.setting_window.log_entry.insert(0, current_log_folder)
+        self.display_max_token_slider(max_token != '0', max_token)
+        self.setting_window.log_toggle_button.set_state(True if current_log_state == "1" else False)
+
+        if typewriter_effect_state is not None and typewriter_effect_state != "":
+            self.setting_window.type_effect_toggle_button.set_state(True if typewriter_effect_state == "1" else False)
+        self.check_input_text()
+
+    def on_apply(self):
+        self.set_setting()
+
+    def open_advanced_window(self, event):
+        event_bus.publish("OpenAdvancedLogWindow")
+
+
+    def select_directory(self, event):
+        current_log_folder = self.setting_window.log_entry.get()
+        default_log_folder = Path(get_documents_directory())/APP_NAME/LOG_FOLDER
+        if not current_log_folder:
+            default_log_dir = Path(default_log_folder)
+            default_log_dir.mkdir(parents=True, exist_ok=True)
+            current_log_folder = default_log_folder
+        # 创建根窗口并隐藏
+        root = tk.Tk()
+        root.withdraw()  # 隐藏主窗口
+
+        # 创建一个顶层窗口
+        top = tk.Toplevel(root)
+        top.overrideredirect(True)
+        top.title("选择目录")
+        top.geometry("0x0")
+        top.attributes("-topmost", True)  # 设置顶层窗口始终在最前
+        #center_window(top, self.root, 10, 10)
+        # 调用 askdirectory，选中文件夹
+        directory = filedialog.askdirectory(parent=top, title="选择目录", initialdir=current_log_folder)
+
+        # 销毁顶层窗口
+        top.destroy()
+        if not directory:
+            directory = current_log_folder
+        self.setting_window.log_entry.delete(0, tk.END)
+        self.setting_window.log_entry.insert(0, directory)
+
+
+    def set_setting(self):
+        api_key = self.setting_window.api_key_input_text_var.get()
+        max_token = self.setting_window.max_token_slider.get()
+        current_log_folder = self.setting_window.log_entry.get()
+        max_token_state = self.settings_window.max_token_toggle_button.get_state()
+        if not max_token_state:
+            max_token = '0'
+        typewriter_effect_state = self.setting_window.type_effect_toggle_button.get_state()
+        log_state = self.setting_window.log_toggle_button.get_state()
+        self.app_config.set("api_key", api_key)
+        self.app_config.set("max_token", str(max_token))
+        self.app_config.set("typewriter_effect", "1" if typewriter_effect_state else "0")
+        self.app_config.set("log_folder", current_log_folder)
+        self.app_config.set("log_state", "1" if log_state else "0")
+        logger.reload_config()
+        event_bus.publish("DialogSettingChanged")
+
+    def check_input_text(self, event=None):
+        api_key = self.setting_window.api_key_input_text_var.get()
+        if api_key:
+            self.setting_window.test_api_key_button.config(state=tk.NORMAL)
+        else:
+            self.setting_window.test_api_key_button.config(state=tk.DISABLED)
+
+
+    def on_click_test_api_key_button(self, event=None):
+        success = True
+        try:
+            api_key = self.setting_window.api_key_input_text_var.get()
+            self.openai_text_api.test(api_key)
+        except Exception as e:
+            CustomConfirmDialog(parent=self.main_window, title="错误", message= "无法通信, 请检查Api Key与网络")
+            success = False
+        if success:
+            CustomConfirmDialog(parent=self.main_window, title="提示", message= "通信正常")
+        self.main_window.grab_set()
+
+    def update_setting_widget(self):
+        pass
+
+    def on_click_reset_button(self, event):
+        dialog = ConfirmDialog(self.main_window, title="确认重置", message="确定要重置吗？")
+        if dialog.result:
+            event_bus.publish("ResetSetting")
+
+    def on_confirm(self):
+        self.set_setting()
+        self.main_window.grab_release()
+        self.main_window.withdraw()
+
+    def on_cancel(self):
+        self.main_window.grab_release()
+        self.main_window.withdraw()
