a5002b24c25f777ae75b232093be7b7c75cddfc9
jim <455428781@qq.com>
2025-02-01 11:58:27 +0800

    add load manager, db data initializer, top bar, setting window, advanced log window, adjust the width of the root window

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import tkinter as tk
+
+from config.app_config import AppConfig
+from db.data_initializer import DataInitializer
 from db.database import init_db
-from db.init_data import insert_default_content_hierarchy
 from db.init_folder import init_folder
+from event.advanced_log_window_manager import AdvancedLogWindowManager
 from event.input_manager import InputManager
 from event.list_manager import ListManager
 from event.main_manager import MainManager
 from event.output_manager import OutputManager
+from event.setting_window_manager import SettingWindowManager
+from event.top_bar_manager import TopBarManager
 from event.tree_manager import TreeManager
+from ui.advanced_log_window import AdvancedLogWindow
 from ui.main_window import MainWindow
+from util.load_manager import LoadManager
 from util.window_util import center_window
 
 
 def initialize_application():
     init_folder()
     init_db()
-    insert_default_content_hierarchy()
+    LoadManager()
+    data_initializer = DataInitializer()
+    data_initializer.initialize()
 
 def main():
     initialize_application()
     root = tk.Tk()
     root.title("openai chat")
-    root.geometry("800x620")
-    center_window(root, 800, 630)
+    root.geometry("1000x620")
+    center_window(root, None,1000, 630)
+
     main_window = MainWindow(root)
+    TopBarManager(main_window)
+    SettingWindowManager(main_window)
+    AdvancedLogWindowManager(main_window)
     MainManager(main_window)
     ListManager(main_window)
+    OutputManager(main_window)
     TreeManager(main_window)
     InputManager(main_window)
-    OutputManager(main_window)
+
+
     root.mainloop()
-
-
 if __name__ == "__main__":
     main()
