01aa74e4762d522a0e83e6776e370ef0399c7415
jim <455428781@qq.com>
2025-02-01 11:43:52 +0800

    add dialogue_preset, dialogue_preset_detail, dialogue_model models

--- db/models.py
+++ db/models.py
@@ -37,6 +37,7 @@ class ContentHierarchy(Base):
     parent_id = Column(Integer, nullable=True)
     child_id = Column(Integer, nullable=False)
     level = Column(Integer, nullable=False, default=0)
+    preset_id = Column(Integer, nullable=True)
     create_time = Column(DateTime, default=datetime.now)
     delete_time = Column(DateTime, nullable=True, default=None)
 
@@ -46,5 +47,31 @@ class Config(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     key = Column(String(255), nullable=False)
     value = Column(String(255), nullable=False)
+    create_time = Column(DateTime, default=datetime.now)
+    delete_time = Column(DateTime, nullable=True, default=None)
+
+
+class DialoguePreset(Base):
+    __tablename__ = 'dialogue_preset'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    max_history_count = Column(Integer, nullable=True, default=0)
+    create_time = Column(DateTime, default=datetime.now)
+    delete_time = Column(DateTime, nullable=True, default=None)
+
+class DialoguePresetDetail(Base):
+    __tablename__ = 'dialogue_preset_detail'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    preset_id = Column(Integer, nullable=True)
+    value = Column(String)
+    create_time = Column(DateTime, default=datetime.now)
+    delete_time = Column(DateTime, nullable=True, default=None)
+
+
+class DialogueModel(Base):
+    __tablename__ = 'dialogue_model'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    type = Column(String, nullable=False)
     create_time = Column(DateTime, default=datetime.now)
     delete_time = Column(DateTime, nullable=True, default=None)
