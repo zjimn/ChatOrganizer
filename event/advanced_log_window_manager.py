baea7d1b7d2c774bf82b5c55645452d5e6b58f79
jim <455428781@qq.com>
2025-02-01 14:58:44 +0800

    change the config

--- event/advanced_log_window_manager.py
+++ event/advanced_log_window_manager.py
@@ -1,15 +1,5 @@
-from pathlib import Path
-from tkinter import font, messagebox, filedialog
-import tkinter as tk
-
-from exceptiongroup import catch
-
-from api.openai_text_api import OpenaiTextApi
-from config.app_config import AppConfig
-from config.constant import APP_NAME, LOG_FOLDER
 from event.event_bus import event_bus
-from util.file_util import get_documents_directory
-from util.global_variables import GlobalVariables
+from util.config_manager import ConfigManager
 from util.load_manager import load_manager
 from util.logger import Logger, logger
 from util.window_util import center_window
@@ -18,7 +8,7 @@ from util.window_util import center_window
 class AdvancedLogWindowManager:
     def __init__(self, main_window):
         self.advanced_log_window = main_window.advanced_log_window
-        self.app_config = AppConfig()
+        self.config_manager = ConfigManager()
         self.root = main_window.root
         self.main_window = self.advanced_log_window.main_window
         self.bind_events()
@@ -40,12 +30,12 @@ class AdvancedLogWindowManager:
         self.load_setting()
 
     def load_setting(self):
-        info_log_state = GlobalVariables.get("info_log_state", True)
-        warn_log_state = GlobalVariables.get("warn_log_state", True)
-        error_log_state = GlobalVariables.get("error_log_state", True)
-        sql_log_state = GlobalVariables.get("sql_log_state", True)
-        request_log_state = GlobalVariables.get("request_log_state", True)
-        response_log_state = GlobalVariables.get("response_log_state", True)
+        info_log_state = self.config_manager.get("info_log_state", True)
+        warn_log_state = self.config_manager.get("warn_log_state", True)
+        error_log_state = self.config_manager.get("error_log_state", True)
+        sql_log_state = self.config_manager.get("sql_log_state", True)
+        request_log_state = self.config_manager.get("request_log_state", True)
+        response_log_state = self.config_manager.get("response_log_state", True)
 
         self.advanced_log_window.info_log_toggle_button.set_state(info_log_state)
         self.advanced_log_window.warn_log_toggle_button.set_state(warn_log_state)
@@ -65,12 +55,12 @@ class AdvancedLogWindowManager:
         request_log_state = self.advanced_log_window.request_log_toggle_button.get_state()
         response_log_state = self.advanced_log_window.response_log_toggle_button.get_state()
 
-        GlobalVariables.set("info_log_state", info_log_state)
-        GlobalVariables.set("warn_log_state", warn_log_state)
-        GlobalVariables.set("error_log_state", error_log_state)
-        GlobalVariables.set("sql_log_state", sql_log_state)
-        GlobalVariables.set("request_log_state", request_log_state)
-        GlobalVariables.set("response_log_state", response_log_state)
+        self.config_manager.set("info_log_state", info_log_state)
+        self.config_manager.set("warn_log_state", warn_log_state)
+        self.config_manager.set("error_log_state", error_log_state)
+        self.config_manager.set("sql_log_state", sql_log_state)
+        self.config_manager.set("request_log_state", request_log_state)
+        self.config_manager.set("response_log_state", response_log_state)
 
         logger.reload_config()
 
