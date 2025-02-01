411e24a4fcf2be2fa7a92b7c930cbfe32e47d9e6
jim <455428781@qq.com>
2025-02-01 11:25:43 +0800

    add tree service

---
+++ service/TreeService.py
@@ -0,0 +1,28 @@
+from db.content_hierarchy_access import ContentHierarchyDataAccess
+from db.dialogue_preset_access import DialoguePresetAccess
+from db.dialogue_preset_detail_access import DialoguePresetDetailAccess
+
+
+class TreeService:
+    def __init__(self):
+        pass
+
+    def update_preset(self, tree_id, preset_id):
+        with ContentHierarchyDataAccess() as cha:
+            cha.update_data(child_id=tree_id,  preset_id = preset_id)
+
+    def delete_preset(self, tree_id):
+        with ContentHierarchyDataAccess() as cha:
+            cha.delete_preset(child_id=tree_id)
+
+    def get_preset_by_tree_id(self, id):
+        with ContentHierarchyDataAccess() as cha:
+            data = cha.get_data_by_child_id(id)
+            if data:
+                return data.preset_id
+
+    def get_name_by_tree_id(self, id):
+        with ContentHierarchyDataAccess() as cha:
+            data = cha.get_data_by_child_id(id)
+            if data:
+                return data.name
