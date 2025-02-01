d3a1930755047f3b4e19cb0e3278e7459b3fda3c
jim <455428781@qq.com>
2025-02-01 12:18:59 +0800

    add init default config

--- util/load_manager.py
+++ util/load_manager.py
@@ -1,6 +1,11 @@
-from config.constant import APP_NAME
+from config.constant import APP_NAME, IMAGE_DIR_PATH
 from util.config_manager import ConfigManager
 from util.global_variables import GlobalVariables
+
+
+def _init_default():
+    image_dir_path =  GlobalVariables.get("image_dir_path", IMAGE_DIR_PATH)
+    GlobalVariables.set("image_dir_path", image_dir_path)
 
 
 class LoadManager:
@@ -12,6 +17,7 @@ class LoadManager:
     def load(self):
         for key, value in self.config_manager.config.items():
             GlobalVariables.set(key, value)
+        _init_default()
 
     def save(self):
         for key, value in GlobalVariables.get_global_dict().items():
