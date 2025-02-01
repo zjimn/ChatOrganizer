4164928a3b2699ea4ce1dcd0f07bd1e2c90b6d42
jim <455428781@qq.com>
2025-02-01 14:52:22 +0800

    Change to use the singleton pattern, add init default config

--- util/config_manager.py
+++ util/config_manager.py
@@ -4,12 +4,28 @@ import json
 import os
 from tkinter import messagebox
 
+from config.constant import APP_NAME, IMAGE_DIR_PATH
+from util.global_variables import GlobalVariables
+
 
 class ConfigManager:
-    def __init__(self, app_name, config_filename="config.json"):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(ConfigManager, cls).__new__(cls)
+            cls._instance.__initialized = False
+        return cls._instance
+
+    def __init__(self, app_name = APP_NAME, config_filename="config.json"):
+        if self.__initialized:
+            return
         self.config_path = Path(os.getenv("APPDATA")) / app_name / config_filename
         self.config_path.parent.mkdir(parents=True, exist_ok=True)
         self.config = self._load_config()
+        GlobalVariables.set_variables(self.config)
+        _init_default()
+        self.__initialized = True
 
     def _load_config(self):
         if not self.config_path.exists():
@@ -21,20 +37,18 @@ class ConfigManager:
                 messagebox.showerror("错误", f"获取配置文件失败: {e}")
         return {}
 
-
-
     def get(self, key, default=None):
-        return self.config.get(key, default)
+        return GlobalVariables.get(key, default)
 
     def set(self, key, value):
-        self.config[key] = value
+        GlobalVariables.set(key, value)
 
     def save(self):
         previous_config = self._load_config()  # fetch the current saved config
         success = True
         with self.config_path.open("w", encoding="utf-8") as file:
             try:
-                json.dump(self.config, file, ensure_ascii=False, indent=4)
+                json.dump(GlobalVariables.get_variables(), file, ensure_ascii=False, indent=4)
             except Exception as e:
                 success = False
                 messagebox.showerror("错误", f"配置文件保存失败: {e}")
@@ -48,3 +62,8 @@ class ConfigManager:
                     messagebox.showerror("错误", f"恢复配置文件失败: {e}")
                     print(f"Error: save file error{e}")
 
+def _init_default():
+    image_dir_path =  GlobalVariables.get("image_dir_path", IMAGE_DIR_PATH)
+    GlobalVariables.set("image_dir_path", image_dir_path)
+    image_dir_path =  GlobalVariables.get("image_dir_path", IMAGE_DIR_PATH)
+    GlobalVariables.set("image_dir_path", image_dir_path)
