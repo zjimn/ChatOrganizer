c89d515cc0414fcbeaae72e999cdcd8b72e8de4b
jim <455428781@qq.com>
2025-02-01 11:33:34 +0800

    add new chat button

--- ui/input_frame.py
+++ ui/input_frame.py
@@ -7,16 +7,16 @@ from util.undo_redo_text import UndoRedoText
 class InputFrame:
     def __init__(self, parent):
         self.parent = parent
-        self.style = ttk.Style()
-        self.style.configure("Custom.TEntry", padding=(3, 5))
+        # self.style = ttk.Style()
+        # self.style.configure("Custom.TEntry", padding=(3, 5))
         self.size_options = ["1024x1024", "1792x1024", "1024x1792"]
         self.type_options = ["文字", "图片"]
         self.submit_button_initial_text = "获取回答"
         self.submit_button_changed_text = "⬛"
         self.submit_button_is_changed = False
-        self.frame = tk.Frame(self.parent)
+        self.frame = ttk.Frame(self.parent)
         self.frame.pack(side=tk.BOTTOM, fill=tk.X, padx=20, pady=(5, 20))
-        self.input_text = UndoRedoText(self.frame, width=10, height=1, padx=5, pady=5)
+        self.input_text = UndoRedoText(self.frame, wrap='word', width=10, height=1, padx=5, pady=5)
         self.input_text.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=(0, 10))
         self.input_text.config(font=("Microsoft YaHei", 10))
         self.submit_button = ttk.Button(self.frame, text="获取回答", state=tk.DISABLED)
@@ -27,4 +27,6 @@ class InputFrame:
         self.size_var = tk.StringVar(value="1024x1024")
         self.size_menu = ttk.OptionMenu(self.frame, self.size_var, "1024x1024", *self.size_options)
         self.size_menu.pack(side=tk.RIGHT, padx=5, anchor=tk.S)
+        self.new_chat_button = ttk.Button(self.frame, text="新的对话", state=tk.NORMAL)
+        self.new_chat_button.pack(side=tk.RIGHT, padx=(0, 10), anchor=tk.S)
         self.size_menu.pack_forget()
