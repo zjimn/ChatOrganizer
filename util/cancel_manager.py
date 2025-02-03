49d068d47cc76f6e776b53326b870929e22ed1b3
jim <455428781@qq.com>
2025-02-03 12:19:30 +0800

    add cancel request manager

---
+++ util/cancel_manager.py
@@ -0,0 +1,22 @@
+from util.logger import logger
+
+
+class CancelManager:
+    running_threads = []
+    @staticmethod
+    def add_running(thread_id):
+        CancelManager.running_threads.append(thread_id)
+
+    @staticmethod
+    def remove_running(thread_id):
+        CancelManager.running_threads.remove(thread_id)
+
+    @staticmethod
+    def remove_all_running():
+        if CancelManager.running_threads:
+            logger.log('debug', f"canceled request thread_ids: {CancelManager.running_threads}")
+            CancelManager.running_threads.clear()
+
+    @staticmethod
+    def check_running_state(thread_id):
+        return thread_id in CancelManager.running_threads
