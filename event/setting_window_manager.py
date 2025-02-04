0a316f5d5b0ed70c9b467b48a8bb0858276d7827
jim <455428781@qq.com>
2025-02-04 17:05:57 +0800

    add model server select function, add set image save folder function

--- event/setting_window_manager.py
+++ event/setting_window_manager.py
@@ -1,16 +1,18 @@
+import os
 from pathlib import Path
-from tkinter import font, filedialog
+from tkinter import font, filedialog, messagebox
 import tkinter as tk
 
-
-from api.openai_text_api import OpenaiTextApi
-from config.constant import APP_NAME, LOG_FOLDER
+from config.constant import APP_NAME, LOG_FOLDER, IMG_FOLDER, OPENAI_SERVER_KEY, OLLAMA_SERVER_KEY
 from event.event_bus import event_bus
+from exception.chat_request_error import ChatRequestError
+from service.model_server_detail_service import ModelServerDetailService
+from service.model_server_service import ModelServerService
+from util.chat_factory import ChatFactory
 from util.config_manager import ConfigManager
 from util.file_util import get_documents_directory
-from util.logger import Logger, logger
+from util.logger import logger
 from util.window_util import center_window
-from widget.confirm_dialog import ConfirmDialog
 from widget.custom_confirm_dialog import CustomConfirmDialog
 
 
@@ -22,23 +24,38 @@ class SettingWindowManager:
         self.root = self.settings_window.root
         self.main_window = self.settings_window.main_window
         self.setting_window = self.settings_window
-        self.openai_text_api = OpenaiTextApi()
+        self.chat_factory = ChatFactory()
+        self.chat_text_api = None
         self.bind_events()
         self.open_setting_if_api_key_is_none()
+        self.model_server_service = ModelServerService()
+        self.model_server_detail_service = ModelServerDetailService()
+        self.model_server_keys = []
+        self.model_server_names = []
+        self.init_model_servers()
 
 
     def bind_events(self):
         self.setting_window.main_window.protocol("WM_DELETE_WINDOW", self.on_cancel)
-        event_bus.subscribe("UpdateSettingWidget", self.update_setting_widget)
         event_bus.subscribe("OpenSettingWindow", self.open_setting_window)
         self.setting_window.confirm_button.config(command = self.on_confirm)
         self.setting_window.cancel_button.config(command = self.on_cancel)
         self.setting_window.test_api_key_button.config(command = self.on_click_test_api_key_button)
-        self.setting_window.api_key_input_text.bind("<KeyRelease>", self.check_input_text)
+        self.setting_window.api_input_text.bind("<Key>", self.check_input_text)
+        self.setting_window.api_input_text.bind("<KeyRelease>", self.check_input_text)
+        self.setting_window.api_input_text.bind("<<UpdatePlaceholder>>", self.check_input_text)
         self.setting_window.max_token_toggle_button.bind("<<CheckboxToggled>>", self.on_max_token_toggle_change)
-        self.setting_window.select_folder_button.bind("<Button-1>", lambda e: self.select_directory(e))
+        self.setting_window.select_log_folder_button.bind("<Button-1>", lambda e: self.select_log_folder_directory(e))
+        self.setting_window.select_img_folder_button.bind("<Button-1>", lambda e: self.select_img_folder_directory(e))
         self.setting_window.advanced_button.bind("<Button-1>", lambda e: self.open_advanced_window(e))
 
+        self.setting_window.model_server_combobox.bind("<<ComboboxSelected>>", self.update_api_input_text)
+
+    def init_model_servers(self):
+        keys, names = self.model_server_service.get_all_list()
+        self.model_server_keys = keys
+        self.model_server_names = names
+        self.setting_window.model_server_combobox['values'] = names
 
     def display_max_token_slider(self, state, max_token_count):
         if state:
@@ -75,15 +92,20 @@ class SettingWindowManager:
         self.load_setting()
 
     def load_setting(self):
-        api_key = self.config_manager.get("api_key")
+        model_server_index = self.config_manager.get("model_server_index", 0)
+        model_server_key = self.model_server_keys[model_server_index]
+        logger.log("debug", f"model server: {model_server_key}")
         max_token = self.config_manager.get("max_token", 0)
         typewriter_effect_state = self.config_manager.get("typewriter_effect", True)
         current_log_folder = self.config_manager.get("log_folder")
+        current_img_folder = self.config_manager.get("img_folder")
         current_log_state = self.config_manager.get("log_state", False)
-        self.setting_window.api_key_input_text_var.set(value="")
-        if api_key:
-            self.setting_window.api_key_input_text_var.set(value=api_key)
+        self.setting_window.model_var.set(self.model_server_names[model_server_index])
+        self.update_api_input_text(update_config=True)
         self.setting_window.max_token_toggle_button.set_state(max_token != 0)
+        self.setting_window.img_entry.delete(0, tk.END)
+        if current_img_folder:
+            self.setting_window.img_entry.insert(0, current_img_folder)
         self.setting_window.log_entry.delete(0, tk.END)
         if current_log_folder:
             self.setting_window.log_entry.insert(0, current_log_folder)
@@ -91,35 +113,74 @@ class SettingWindowManager:
         self.setting_window.log_toggle_button.set_state(current_log_state)
 
         self.setting_window.type_effect_toggle_button.set_state(typewriter_effect_state)
-        self.check_input_text()
 
     def open_advanced_window(self, event):
         event_bus.publish("OpenAdvancedLogWindow")
 
 
-    def select_directory(self, event):
+    def update_api_input_text(self, event = None, update_config = False):
+        api_key = ""
+        api_url = ""
+        selected_model_server_index = self.setting_window.model_server_combobox.current()
+        model_server_key = self.model_server_keys[selected_model_server_index]
+        data = self.model_server_detail_service.get_data_by_server_key(model_server_key)
+        if data:
+            api_key = data.api_key
+            api_url = data.api_url
+        if api_key is None:
+            api_key = ""
+        if api_url is None:
+            api_url = ""
+        if self.is_api_key():
+            self.setting_window.api_label.config(text="API Key ")
+            self.setting_window.api_input_text_var.set(value=api_key)
+        else:
+            self.setting_window.api_label.config(text="请求地址")
+            self.setting_window.api_input_text_var.set(value=api_url)
+        self.check_input_text()
+        if update_config:
+            self.config_manager.set("api_key", api_key)
+            self.config_manager.set("api_url", api_url)
+
+    def is_api_key(self):
+        selected_model_server_index = self.setting_window.model_server_combobox.current()
+        return not self.model_server_keys[selected_model_server_index] in [OLLAMA_SERVER_KEY]
+
+    def select_img_folder_directory(self, event):
+        current_img_folder = self.setting_window.img_entry.get()
+        default_img_folder = Path(get_documents_directory())/APP_NAME/IMG_FOLDER
+        if not current_img_folder:
+            default_img_save_dir = Path(default_img_folder)
+            default_img_save_dir.mkdir(parents=True, exist_ok=True)
+            current_img_folder = default_img_folder
+        directory = self.get_directory(default_img_folder)
+        if not directory:
+            directory = current_img_folder
+        self.setting_window.img_entry.delete(0, tk.END)
+        self.setting_window.img_entry.insert(0, directory)
+
+    def get_directory(self, initialdir = None):
+        root = tk.Tk()
+        root.withdraw()
+        top = tk.Toplevel(root)
+        top.overrideredirect(True)
+        top.title("选择目录")
+        top.geometry("0x0")
+        top.attributes("-topmost", True)
+        directory = filedialog.askdirectory(parent=top, title="选择目录", initialdir=initialdir)
+        top.destroy()
+        if directory:
+            directory = os.path.normpath(directory)
+        return directory
+
+    def select_log_folder_directory(self, event):
         current_log_folder = self.setting_window.log_entry.get()
         default_log_folder = Path(get_documents_directory())/APP_NAME/LOG_FOLDER
         if not current_log_folder:
             default_log_dir = Path(default_log_folder)
             default_log_dir.mkdir(parents=True, exist_ok=True)
             current_log_folder = default_log_folder
-        # 创建根窗口并隐藏
-        root = tk.Tk()
-        root.withdraw()  # 隐藏主窗口
-
-        # 创建一个顶层窗口
-        top = tk.Toplevel(root)
-        top.overrideredirect(True)
-        top.title("选择目录")
-        top.geometry("0x0")
-        top.attributes("-topmost", True)  # 设置顶层窗口始终在最前
-        #center_window(top, self.root, 10, 10)
-        # 调用 askdirectory，选中文件夹
-        directory = filedialog.askdirectory(parent=top, title="选择目录", initialdir=current_log_folder)
-
-        # 销毁顶层窗口
-        top.destroy()
+        directory = self.get_directory(default_log_folder)
         if not directory:
             directory = current_log_folder
         self.setting_window.log_entry.delete(0, tk.END)
@@ -127,9 +188,11 @@ class SettingWindowManager:
 
 
     def set_setting(self):
-        api_key = self.setting_window.api_key_input_text_var.get()
+        selected_model_server_index = self.setting_window.model_server_combobox.current()
+        api_input = self.setting_window.api_input_text_var.get()
         max_token = self.setting_window.max_token_slider.get()
         current_log_folder = self.setting_window.log_entry.get()
+        current_img_folder = self.setting_window.img_entry.get()
         max_token_state = self.settings_window.max_token_toggle_button.get_state()
         if not max_token_state:
             max_token = 0
@@ -139,18 +202,43 @@ class SettingWindowManager:
         if log_state and not current_log_folder:
             CustomConfirmDialog(parent=self.main_window, title="警告", message="请选择日志保存目录")
             return False
-
+        current_model_server_key = self.model_server_keys[selected_model_server_index]
+        self.config_manager.set("model_server_index", selected_model_server_index)
+        self.config_manager.set("model_server_key", current_model_server_key)
+        data = self.model_server_detail_service.get_data_by_server_key(current_model_server_key)
+        txt_model_id = None
+        img_model_id = None
+        if data:
+            txt_model_id = data.txt_model_id
+            img_model_id = data.img_model_id
+        self.config_manager.set("txt_model_id", txt_model_id)
+        self.config_manager.set("img_model_id", img_model_id)
+        if txt_model_id is None:
+            self.config_manager.set("txt_model_name", None)
+        if img_model_id is None:
+            self.config_manager.set("img_model_name", None)
+        event_bus.publish("ModelServerChange")
+        api_key = api_input
+        api_url = api_input
+        if self.is_api_key():
+            api_url = None
+        else:
+            api_key = None
+        self.model_server_detail_service.update_or_insert_data(current_model_server_key, api_key = api_key, api_url = api_url)
         self.config_manager.set("api_key", api_key)
+        self.config_manager.set("api_url", api_url)
         self.config_manager.set("max_token", max_token)
         self.config_manager.set("typewriter_effect", typewriter_effect_state)
+        self.config_manager.set("img_folder", current_img_folder)
         self.config_manager.set("log_folder", current_log_folder)
         self.config_manager.set("log_state", log_state)
+        logger.log("debug", f"config: {self.config_manager.config}")
         logger.reload_config()
         event_bus.publish("DialogSettingChanged")
         return True
 
     def check_input_text(self, event=None):
-        api_key = self.setting_window.api_key_input_text_var.get()
+        api_key = self.setting_window.api_input_text_var.get()
         if api_key:
             self.setting_window.test_api_key_button.config(state=tk.NORMAL)
         else:
@@ -158,24 +246,21 @@ class SettingWindowManager:
 
 
     def on_click_test_api_key_button(self, event=None):
-        success = True
-        try:
-            api_key = self.setting_window.api_key_input_text_var.get()
-            self.openai_text_api.test(api_key)
-        except Exception as e:
-            CustomConfirmDialog(parent=self.main_window, title="错误", message= "无法通信, 请检查Api Key与网络")
-            success = False
-        if success:
-            CustomConfirmDialog(parent=self.main_window, title="提示", message= "通信正常")
+        api_input = self.setting_window.api_input_text_var.get()
+        selected_model_server_index = self.setting_window.model_server_combobox.current()
+        selected_model_server_key = self.model_server_keys[selected_model_server_index]
         self.main_window.grab_set()
-
-    def update_setting_widget(self):
-        pass
-
-    def on_click_reset_button(self, event):
-        dialog = ConfirmDialog(self.main_window, title="确认重置", message="确定要重置吗？")
-        if dialog.result:
-            event_bus.publish("ResetSetting")
+        success = False
+        try:
+            self.chat_factory.test(selected_model_server_key, api_input)
+            success = True
+        except ChatRequestError as cre:
+            CustomConfirmDialog(parent= self.main_window, title="错误", message="无法通信, 请检查API Key与网络")
+        except Exception as e:
+            logger.log('error', e)
+            messagebox.showerror("错误", f"请求异常:\n{e}")
+        if success:
+            CustomConfirmDialog(parent= self.main_window, title="提示", message="通信正常")
 
     def on_confirm(self):
         result = self.set_setting()
