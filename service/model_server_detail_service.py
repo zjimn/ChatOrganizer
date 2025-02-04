687b8cf9f021160849cdfe27a665b32b2ba43893
jim <455428781@qq.com>
2025-02-04 16:44:37 +0800

    add model_server_detail service

---
+++ service/model_server_detail_service.py
@@ -0,0 +1,25 @@
+from db.model_server_detail_access import ModelServerDetailAccess
+
+
+class ModelServerDetailService:
+    def __init__(self):
+        pass
+
+    def get_all_dialog_model_list(self, type = "txt", server_key = None):
+        with ModelServerDetailAccess() as msda:
+            return msda.get_all_data(type, server_key)
+
+    def get_data_by_server_key(self, server_key):
+        with ModelServerDetailAccess() as msda:
+            data = msda.get_data_by_server_key(server_key)
+            return data
+
+    def update_or_insert_data(self, server_key: str, txt_model_id: str = None, img_model_id: str = None, api_key: str = None, api_url: str = None):
+        with ModelServerDetailAccess() as msda:
+            msda.upsert(server_key, txt_model_id, img_model_id, api_key, api_url)
+
+    def delete_data(self, server_key):
+        if not server_key:
+            return
+        with ModelServerDetailAccess() as msda:
+            msda.delete(server_key)
