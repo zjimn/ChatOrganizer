097ba7f96eaced5321376a3a680d817452dc1191
jim <455428781@qq.com>
2025-02-04 16:48:16 +0800

    add server param

--- service/dialog_model_service.py
+++ service/dialog_model_service.py
@@ -6,20 +6,20 @@ class DialogueModelService:
     def __init__(self):
         pass
 
-    def get_all_dialog_model_list(self, type = "text"):
+    def get_all_dialog_model_list(self, type = "txt", server_key = None):
         with DialogueModelAccess() as dpa:
-            return dpa.get_all_data(type)
+            return dpa.get_all_data(type, server_key)
 
     def get_data_by_id(self, id):
         with DialogueModelAccess() as dpa:
             data = dpa.get_data_by_id(id)
             return data
 
-    def update_or_insert_data(self, detail, type = "text"):
+    def update_or_insert_data(self, detail, type = "txt", server = None):
         with DialogueModelAccess() as dma:
             for data in detail:
                 if data.id is None:
-                    dma.insert_data(data.name, type)
+                    dma.insert_data(data.name, type, server)
                 else:
                     dma.update_data(data.id, data.name, data.delete_time)
 
